c961ead779beb303c1dc624bd2b961738c22ffd5
Het Modha <hdmodha15@gmail.com>
2025-04-06 16:17:46 +0200

    add: registration route and request handlers

---
+++ routes/register.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/go-rest/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the event id!"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch the event"})
+	}
+
+	userId := context.GetInt64("userId")
+	err = event.Register(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not register for the event"})
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "Registered for the event"})
+
+}
+
+func cancelRegistration(context *gin.Context) {
+
+}
